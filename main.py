e63dda125afb0c1b752cbc1f2cd72c5c7cc071a8
Soulbolt <soulbolt@gmail.com>
2024-01-07 11:04:39 -0700

    scraped data to retrieve links, addresseses, and prices from the listing. Each cleaned up and on their separate lists.

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from bs4 import BeautifulSoup
+import requests
 from selenium import webdriver
 
-soup = BeautifulSoup()
+response = requests.get(url="https://appbrewery.github.io/Zillow-Clone/")
+response.raise_for_status
+zillow_clone_website = response.text
+
+soup = BeautifulSoup(zillow_clone_website, "html.parser")
 driver = webdriver.Chrome()
+
+listing = soup.find_all(name="a", class_="property-card-link")
+listing_links = [link.get("href") for link in listing]
+print(f"Listing of Links : {listing_links}")
+addresses = soup.find_all(name="a", class_="StyledPropertyCardDataArea-anchor")
+address_list = [address.text.replace(" | ", "").strip() for address in addresses]
+print(f"Address List: {address_list}")
+prices = soup.find_all(name="span", class_="PropertyCardWrapper__StyledPriceLine")
+price_list = [price.text[:6] for price in prices]
+print(f"Price list: {price_list}")
